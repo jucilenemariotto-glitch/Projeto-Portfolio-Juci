b2646fde5f04a46763f253e5d20c295f20a90e7c
jucilenemariotto-glitch <jucilene.mariotto@escola.pr.gov.br>
2025-11-17 19:29:19 -0300

    prompt

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ texto.textContent = "Jucilene";
 
 let nome = prompt("Qual o seu nome");
 if (texto.textContent == null){
-    texto.textContent ="Seja bem-vindo(a)!" 
+    texto.textContent ="Seja bem-vindo(a)!" ;
 }
 else{
     texto.textContent = nome;

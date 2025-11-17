e81418ffca8d2078864757e0f926a8a2f3d352c8
jucilenemariotto-glitch <jucilene.mariotto@escola.pr.gov.br>
2025-11-17 19:28:41 -0300

    criar css

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ let texto = document.querySelector("#texto");
 texto.textContent = "Jucilene";
 
 let nome = prompt("Qual o seu nome");
+if (texto.textContent == null){
+    texto.textContent ="Seja bem-vindo(a)!" 
+}
+else{
+    texto.textContent = nome;
+}
+
+
+
 texto.textContent = nome;
